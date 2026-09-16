5edd3beec6709cacc423609d12c482b77db54421
Teodor Soltirov <soltirovteo7@yahoo.com>
2026-09-16 14:19:36 +0200

    Klassen- und Methodendokumentation zu GewinnModel hinzugefügt

--- src/model/GewinnModel.java
+++ src/model/GewinnModel.java
@@ -1,11 +1,19 @@
 package model;
 
+/**
+ * Enthält die Spielelogik
+ * @author Teodor Soltirov
+ * @version 2026-09-16
+ */
 public class GewinnModel {
     private int gesamtPunkte;
     private int spielerZahl;
     private int computerZahl;
     private int rundenErgebnis;
 
+    /**
+     * Parameterloser Konstruktor, Spiel beginnt mit 30 Punkten
+     */
     public GewinnModel() {
         this.spielerZahl = 0;
         this.computerZahl = 0;
@@ -19,8 +27,15 @@ public class GewinnModel {
 
     public int getRundenErgebnis() { return rundenErgebnis; }
 
+    /**
+     * Generiert eine zufällige Zahl zwischen 1 und 9 (inkl. 1 und 9) für den Computer
+     */
     public void berechneComputerZahl() { this.computerZahl = (int) (Math.random() * 9) + 1; }
 
+    /**
+     * Überprüft die Eingabe des Spielers und berechnet die Differenz von der Zahl des Computers
+     * @param spielerZahl - Der Guess des Spielers (erlaubt 1 - 9) (inkl. 1 und 9)
+     */
     public void berechneRunde(int spielerZahl) {
         if(spielerZahl > 9 || spielerZahl < 1) return;
         this.spielerZahl = spielerZahl;
@@ -31,10 +46,18 @@ public class GewinnModel {
         this.gesamtPunkte += rundenErgebnis;
     }
 
+    /**
+     * Spieler gewinnt bei Gesamtpunktzahl größer oder gleich 100
+     * @return true wenn >= 100, false wenn Gesamtpunktzahl unter 100
+     */
     public boolean hatGewonnen() {
         return gesamtPunkte >= 100;
     }
 
+    /**
+     * Spieler verliert bei Gesamtpunktzahl kleiner oder gleich 0
+     * @return true wenn <= 0, false wenn Gesamtpunktzahl über 0
+     */
     public boolean hatVerloren() {
         return gesamtPunkte <= 0;
     }
